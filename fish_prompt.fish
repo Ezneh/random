be72ec841abf882f14e827ec2e74129f8e3420ba
Vincent Petit <petitv.isat@gmail.com>
2014-01-12 18:36:15 +0100

    Update to fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,14 +1,20 @@
 function fish_prompt --description 'Write out the prompt'
-   set -l pwd (basename (prompt_pwd))
-   set -l prompt_symbol ''
-   set -l user_color ''
-   switch $USER
-       case root
-            set user_color red
-            set prompt_symbol '#'
-       case '*'
-            set user_color 00FF00
-            set prompt_symbol '$'
-   end
-   printf "%s[%s%s %s%s%s@%s%s%s]%s%s " (set_color white) (set_color 808080) (date "+%H:%M:%S") (set_color $user_color) $USER (set_color C0C0C0) (set_color 0099CC) $pwd (set_color white) (set_color $user_color) $prompt_symbol
+    set -l date_format "+%H:%M:%S"
+    set -l pwd (basename (prompt_pwd))
+    set -l symbol "@"
+    set -l prompt_symbol ''
+    set -l user_color ''
+    set -l brackets_color white
+    set -l date_color 808080
+    set -l symbol_color C0C0C0
+    set -l cwd_color 0099CC
+    switch $USER
+        case root
+             set user_color red
+             set prompt_symbol '#'
+        case '*'
+             set user_color 00FF00
+             set prompt_symbol '$'
+    end
+    printf "%s[%s%s %s%s%s%s%s%s%s]%s%s " (set_color $brackets_color) (set_color $date_color) (date $date_format) (set_color $user_color) $USER (set_color $symbol_color) $symbol (set_color $cwd_color) $pwd (set_color $brackets_color) (set_color $user_color) $prompt_symbol
 end
